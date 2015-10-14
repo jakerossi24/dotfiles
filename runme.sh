8908bf68399c20caf6d6d58454ae97c8cc9e577d
Jake Rossi <jake.rossi@sitepoint.com>
2015-10-14 05:23:47 +0000

    Updated error in bash script

--- runme.sh
+++ runme.sh
@@ -10,10 +10,10 @@ echo "Install complete"
 
 echo "Moving old files to a new folder called old"
 if [ -d $HOME/old ]; then echo 'Exists'; else sudo mkdir $HOME/old; fi
-if [ -f $HOME/.gitconfig ]; then sudo mv $HOME/.gitconfig $HOME/old fi
-if [ -f $HOME/.gitignore_global ]; then sudo mv $HOME/.gitignore_global $HOME/old fi
-if [ -f $HOME/.tmux.conf ]; then sudo mv $HOME/.tmux.conf $HOME/old fi
-if [ -f $HOME/.vimrc ]; then sudo mv $HOME/.vimrc $HOME/old fi
+if [ -f $HOME/.gitconfig ]; then sudo mv $HOME/.gitconfig $HOME/old; fi
+if [ -f $HOME/.gitignore_global ]; then sudo mv $HOME/.gitignore_global $HOME/old; fi
+if [ -f $HOME/.tmux.conf ]; then sudo mv $HOME/.tmux.conf $HOME/old; fi
+if [ -f $HOME/.vimrc ]; then sudo mv $HOME/.vimrc $HOME/old; fi
 sudo echo "Move ~Complete"
 
 echo "Symlink - Start"

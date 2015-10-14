e9fc457b45433f105b756017aafaab5eefd1aeb2
Jake Rossi <jake.rossi@sitepoint.com>
2015-10-14 06:27:22 +0000

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -9,18 +9,18 @@ sudo gem install tmuxinator
 echo "Install complete"
 
 echo "Moving old files to a new folder called old"
-if [ -d $HOME/old ]; then echo 'Exists'; else sudo mkdir $HOME/old; fi
-if [ -f $HOME/.gitconfig ]; then sudo mv $HOME/.gitconfig $HOME/old; fi
-if [ -f $HOME/.gitignore_global ]; then sudo mv $HOME/.gitignore_global $HOME/old; fi
-if [ -f $HOME/.tmux.conf ]; then sudo mv $HOME/.tmux.conf $HOME/old; fi
-if [ -f $HOME/.vimrc ]; then sudo mv $HOME/.vimrc $HOME/old; fi
+if [ -d $HOME/old ]; then echo 'Exists'; else mkdir $HOME/old; fi
+if [ -f $HOME/.gitconfig ]; then mv $HOME/.gitconfig $HOME/old; fi
+if [ -f $HOME/.gitignore_global ]; then mv $HOME/.gitignore_global $HOME/old; fi
+if [ -f $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf $HOME/old; fi
+if [ -f $HOME/.vimrc ]; then mv $HOME/.vimrc $HOME/old; fi
 sudo echo "Move ~Complete"
 
 echo "Symlink - Start"
-sudo ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-sudo ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
-sudo ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-sudo ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 echo "Symlink - Complete"
 
 echo "Vim Setup - Start"

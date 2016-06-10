2e875d43ebf56e7af9e7015aafc89ecf97570bbd
Jake Rossi <jake.rossi@sitepoint.com>
2016-06-10 10:37:45 +1000

    Remove tmux

--- runme.sh
+++ runme.sh
@@ -2,24 +2,18 @@
 
 echo "Updating packages"
 sudo apt-get update
-
-echo "Installing tmux and tmuxinator"
-sudo apt-get install tmux
-sudo gem install tmuxinator
 echo "Install complete"
 
 echo "Moving old files to a new folder called old"
 if [ -d $HOME/old ]; then echo 'Exists'; else mkdir $HOME/old; fi
 if [ -f $HOME/.gitconfig ]; then mv $HOME/.gitconfig $HOME/old; fi
 if [ -f $HOME/.gitignore_global ]; then mv $HOME/.gitignore_global $HOME/old; fi
-if [ -f $HOME/.tmux.conf ]; then mv $HOME/.tmux.conf $HOME/old; fi
 if [ -f $HOME/.vimrc ]; then mv $HOME/.vimrc $HOME/old; fi
 sudo echo "Move ~Complete"
 
 echo "Symlink - Start"
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
-ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 echo "Symlink - Complete"
 
